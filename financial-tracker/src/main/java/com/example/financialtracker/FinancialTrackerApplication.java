09cd91b7e89ae9fc0565447e21032b12333bc9ff
JOELNATHAN544 <wankojoelnathan@gmail.com>
2026-01-17 13:35:26 +0100

    feat: Enable retry functionality for the application.

--- financial-tracker/src/main/java/com/example/financialtracker/FinancialTrackerApplication.java
+++ financial-tracker/src/main/java/com/example/financialtracker/FinancialTrackerApplication.java
@@ -4,16 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EntityScan("com.example.financialtracker.model")
 @EnableJpaRepositories("com.example.financialtracker.repository")
 @EnableScheduling
+@EnableRetry
 public class FinancialTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinancialTrackerApplication.class, args);
 	}
 
-} 
+}
