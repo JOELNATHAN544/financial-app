a7646ae1231b18370adae4db6c22ab53fa356cb1
JOELNATHAN544 <wankojoelnathan@gmail.com>
2026-01-18 15:20:47 +0100

    feat: Enable asynchronous method execution in the application.

--- financial-tracker/src/main/java/com/example/financialtracker/FinancialTrackerApplication.java
+++ financial-tracker/src/main/java/com/example/financialtracker/FinancialTrackerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaRepositories("com.example.financialtracker.repository")
 @EnableScheduling
 @EnableRetry
+@EnableAsync
 public class FinancialTrackerApplication {
 
 	public static void main(String[] args) {
